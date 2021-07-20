24d59f3c46449149eeeaace36e3fced4afda9c33
Kacper <kacpergierycz@gmail.com>
2021-07-20 10:20:06 +0200

    basic stuff

--- Generics/src/generics/GenericsDemo.java
+++ Generics/src/generics/GenericsDemo.java
@@ -1,10 +1,20 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class GenericsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Name 1");
+		names.add("Name 2");
+		
+		String myFirstName=  names.get(0);
+		System.out.println(myFirstName);
+		
 	}
 
 }
